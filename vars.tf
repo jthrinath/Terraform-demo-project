a106520c6c40e4ecb00929170e03ddd16bc7642c
jthrinath <thrinathjdevops@gmail.com>
2022-10-17 20:13:40 +0530

    first commit

---
+++ vars.tf
@@ -0,0 +1,18 @@
+# varables
+
+variable REGION {
+  type = string
+  default = "ap-south-1"
+}
+
+variable EC2-AMI {
+
+    type = string
+    default = "ami-062df10d14676e201"
+  
+}
+
+variable EC2-instance-type {
+   type = string
+   default = "t2.micro"
+}
---
+++ vpc-infra.tf
@@ -0,0 +1,21 @@
+#
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.34.0"
+    }
+  }
+
+   backend "s3" {
+    bucket = "sr3m-project"
+    key    = "state"
+    region = "us-east-1"
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = var.REGION
+}
